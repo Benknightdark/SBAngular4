10c365fb4117cdc05ff0c521157424cc9915ec0c
Kung Ben <Kung Ben>
2017-06-03 12:50:18 +0800

    no shadowdom

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit,ViewChild } from '@angular/core';
+import { Component,OnInit,ViewChild,ViewEncapsulation } from '@angular/core';
 import { Http } from "@angular/http";
 import { Observable } from 'rxjs'
 import 'rxjs'
@@ -6,7 +6,8 @@ import { TreenodeComponent } from "app/shared";
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss']
+    styleUrls: ['./sidebar.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class SidebarComponent {
     constructor(private http: Http) { }
